bb7c262739efb63194628524df8c6fc2871d3688
Lyndon D'Arcy <lyndon.darcy@gmail.com>
2014-07-13 11:51:33 +1000

    WIP - added django models, some api views

--- server/herri/api/admin.py
+++ server/herri/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.models import Attribute, Weighting, AttributeModel
 
 # Register your models here.
+admin.site.register(Attribute)
+admin.site.register(Weighting)
+admin.site.register(AttributeModel)
--- server/herri/herri/urls.py
+++ server/herri/herri/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from api.views import get_attribute_model
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'herri.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^api/attribute_models/new/$', 'api.views.save_attribute_model'),
+    url(r'^api/attributes/$', 'api.views.get_attributes'),
+    url(r'^api/attribute_models/$', 'api.views.get_attribute_models'),
+    url(r'^api/attribute_model/(\d+)/$', 'api.views.get_attribute_model', name='model_id'),
     url(r'^admin/', include(admin.site.urls)),
 )
