1bf70c5192d1708db36a31d027d5e40b461b6fc8
Lyndon D'Arcy <lyndon.darcy@gmail.com>
2014-07-13 12:55:18 +1000

    Added unicode representations of objects in models.

--- server/herri/api/models.py
+++ server/herri/api/models.py
@@ -7,13 +7,23 @@ class Attribute(models.Model):
     column_name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __unicode__(self):
+        return "Attribute: %s (%s)" % (self.name, self.column_name)   
+
 class Weighting(models.Model):
     attribute = models.ForeignKey('Attribute')
     weight = models.FloatField()
     description = models.TextField()
+
+    def __unicode__(self):
+            return "Weighting: {0}={1:.2f} ({2})".format('Total pop', 0.913, 1)
+
 
 class AttributeModel(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     user = models.ForeignKey(User)
     weightings = models.ManyToManyField('Weighting')
+
+    def __unicode__(self):
+        return "Model: %s" % (self.name)   
